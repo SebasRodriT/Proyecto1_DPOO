dcdc395d36c93b80627ae19be7d61d6269ec405f
rodrigopaz03 <r.pazl@uniandes.edu.co>
2024-04-12 07:07:16 -0500

    jre

--- srcEntrega2/uniandes/dpoo/galeria/modelo/plataforma/Plataforma.java
+++ srcEntrega2/uniandes/dpoo/galeria/modelo/plataforma/Plataforma.java
@@ -1,5 +1,24 @@
 package uniandes.dpoo.galeria.modelo.plataforma;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Plataforma {
 
+	private ArrayList<String> usuarios = new ArrayList<String>();
+	private HashMap<String, ArrayList<String>> mapaPagos = new HashMap<String, ArrayList<String>>();
+	
+	public Plataforma() {}
+	
+	public void vender(Comprador comprador, Pieza pieza) {
+	}
+	public void subastar(Comprador comprador, Pieza pieza) {
+		
+	} 
+	public void agregarUsuario(Usuario usuario) {
+		
+	}
+	public void agregarPago(Comprador comprador, int precio) {
+		
+	}
 }
